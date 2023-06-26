307137a2abbcd0ccd49a7e370255c988c9ec1dbc
Makulugia <austex@gmx.com>
2023-06-25 19:10:51 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+namespace Newsler
+{
+    class Article
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Link { get; set; }
+    }
+
+    class NewsContext : DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connectionString = configuration.GetConnectionString("NewsDbConnection");
+
+            // Disable certificate revocation check
+            SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
+            connectionStringBuilder.TrustServerCertificate = true;
+            optionsBuilder.UseSqlServer(connectionStringBuilder.ConnectionString);
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set the path to the ChromeDriver executable
+            string chromeDriverPath = "path/to/chromedriver";
+
+            // Create an instance of the ChromeDriver
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--ignore-certificate-errors");
+
+            ChromeDriverService chromeDriverService = ChromeDriverService.CreateDefaultService(chromeDriverPath);
+            IWebDriver driver = new ChromeDriver(chromeDriverService);
+
+            // Navigate to the desired website
+            driver.Navigate().GoToUrl("https://www.businessnews.com.tn/");
+
+            // Find all the article title elements using Selenium
+            var articleElements = driver.FindElements(By.XPath("//*[@id=\"mm-0\"]/main/div[2]/div[4]/div[2]/div[*]/div/a"));
+
+            if (articleElements.Count > 0)
+            {
+                List<Article> articles = new List<Article>();
+
+                foreach (var articleElement in articleElements)
+                {
+                    string articleTitle = articleElement.Text.Trim();
+                    string articleLink = articleElement.GetAttribute("href");
+
+                    articles.Add(new Article { Title = articleTitle, Link = articleLink });
+                }
+
+                // Store the extracted article titles and links or perform further actions
+                foreach (var article in articles)
+                {
+                    Console.WriteLine($"Title: {article.Title}");
+                    Console.WriteLine($"Link: {article.Link}");
+                }
+
+                using (var dbContext = new NewsContext())
+                {
+                    // Check if articles already exist in the database
+                    var existingArticles = dbContext.Articles.ToList();
+
+                    foreach (var article in articles)
+                    {
+                        // Check if the article already exists in the database
+                        var existingArticle = existingArticles.FirstOrDefault(a => a.Title == article.Title && a.Link == article.Link);
+
+                        if (existingArticle == null)
+                        {
+                            // Article is new, add it to the database
+                            dbContext.Articles.Add(article);
+                        }
+                    }
+
+                    // Save changes to the database
+                    dbContext.SaveChanges();
+                }
+
+                Console.WriteLine("Articles saved to the database.");
+            }
+            else
+            {
+                Console.WriteLine("No articles found.");
+            }
+
+            // Close the browser window
+            driver.Quit();
+
+            // Dispose of the driver object to release resources
+            driver.Dispose();
+        }
+    }
+}
