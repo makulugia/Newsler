89f672c66759249a6ada3057cfa1b93366aa596c
Makulugia <austex@gmx.com>
2023-06-25 22:39:37 -0500

    code split

---
+++ NewsContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Data.SqlClient;
+
+namespace Newsler
+{
+    class NewsContext : DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connectionString = configuration.GetConnectionString("NewsDbConnection");
+
+            // Disable certificate revocation check
+            SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
+            connectionStringBuilder.TrustServerCertificate = true;
+            optionsBuilder.UseSqlServer(connectionStringBuilder.ConnectionString);
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,42 +2,10 @@
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using System.Linq;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using System.Data.SqlClient;
 
 namespace Newsler
 {
-    class Article
-    {
-        public int Id { get; set; }
-        public string? Title { get; set; }
-        public string? Link { get; set; }
-    }
-
-    class NewsContext : DbContext
-    {
-        public DbSet<Article> Articles { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            IConfigurationRoot configuration = new ConfigurationBuilder()
-                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-                .AddJsonFile("appsettings.json")
-                .Build();
-
-            string connectionString = configuration.GetConnectionString("NewsDbConnection");
-
-            // Disable certificate revocation check
-            SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
-            connectionStringBuilder.TrustServerCertificate = true;
-            optionsBuilder.UseSqlServer(connectionStringBuilder.ConnectionString);
-
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
